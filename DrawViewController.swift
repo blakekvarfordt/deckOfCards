8a6630f9c546be98dbf87a62cf115614bbc6c598
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-12 11:01:24 -0600

    adds MVC files

---
+++ DrawViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DrawViewController.swift
+//  CardDeck
+//
+//  Created by Blake kvarfordt on 8/12/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var cardImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func drawButtonTapped(_ sender: Any) {
+    }
+    
+ 
+
+}
