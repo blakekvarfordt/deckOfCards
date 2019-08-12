b72fca9a84ad34fe011400030db49f41b861b89e
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-12 10:56:13 -0600

    sets up UI

--- CardDeck/ViewController.swift
+++ CardDeck/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
